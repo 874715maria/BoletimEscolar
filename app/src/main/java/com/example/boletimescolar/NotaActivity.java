9f05cfa019ae1c61157786d0adb95c32a245faa0
albertolemos@gmail.com <albertolemos@gmail.com>
2020-10-05 18:17:38 -0300

    Reordenação dos nomes dos arquivos de layout (.xml) e criação da classe Java InicioActivity e activity_inicio.xml

--- app/src/main/java/com/example/boletimescolar/NotaActivity.java
+++ app/src/main/java/com/example/boletimescolar/NotaActivity.java
@@ -3,11 +3,7 @@ package com.example.boletimescolar;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.widget.EditText;
@@ -23,7 +19,7 @@ public class NotaActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activiy_cadastrar_boletim);
+        setContentView(R.layout.activity_cadasto_nota);
 
         this.nota = new Nota();
         this.editTextPeriodo = (EditText) findViewById(R.id.editTextPeriodo);
