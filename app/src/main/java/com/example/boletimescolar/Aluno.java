0e0acae199441bcb2ad3987e51eae96bec02f398
Gerferson Carlos <gefersoncarlos624@gmail.com>
2020-10-07 23:46:03 -0300

    tirando umas coisas de algumas telas(atributos, implementando banco)

--- app/src/main/java/com/example/boletimescolar/Aluno.java
+++ app/src/main/java/com/example/boletimescolar/Aluno.java
@@ -1,81 +1,20 @@
 package com.example.boletimescolar;
 
-import java.sql.ResultSet;
-import java.util.ArrayList;
-
 public class Aluno extends _Default {
 
-    private int id;
+    private String id;
     private String nome;
-    private String idade;
     private String email;
-    private String nomeEscola;
+    private String password;
 
-
-
-    public Aluno (){
-    }
-    public Aluno (String nome, String idade, String email, String nomeEscola){
-        super();
-        this.id = id;
-        this.nome = nome;
-        this.idade = idade;
-        this.email = email;
-        this.nomeEscola = nomeEscola;
-    }
-    public Aluno (int id, String nome, String idade, String email, String nomeEscola){
-        super();
-        this.id = id;
-        this.nome = nome;
-        this.idade = idade;
-        this.email = email;
-        this.nomeEscola = nomeEscola;
+    public Aluno() {
     }
 
-   // public ArrayList<Aluno> getLista(){
-
-       // ArrayList<Aluno> lista = new ArrayList<>();
-       // try {
-         //   ResultSet resultSet = select("SELECT * FROM usuario");
-          //  if (resultSet != null){
-             //   while (resultSet.next()){
-               //     Aluno obj = new Aluno();
-                //    obj.setId(resultSet.getInt("id"));
-               //     obj.setNome(resultSet.getString("nome"));
-                 //   obj.setIdade(resultSet.getString("idade"));
-                  //  obj.setEmail(resultSet.getString("email"));
-                  //  obj.setNomeEscola(resultSet.getString("nomeEscola"));
-                  //  lista.add(obj);
-                  //  obj = null;
-               // }
-            //}
-      //  }catch (Exception ex){
-          //  this.mensagem = ex.getMessage();
-        //    this._status = false;
-      //  }
-      //  return lista;
-   // }
-
-    public void salvar(){
-        String comando = "";
-        if (this.getId() == id){
-            comando = String.format("INSERT INTO Aluno(nome, idade, email, nomeEscola);",
-                    this.getNome(), this.getIdade(), this.getEmail(), this.getNomeEscola());
-        }else {
-            comando = String.format("UPDATE Aluno SET nome = nome, idade = idade, email = email, nomeEscola = nomeEscola WHERE id = id;",
-                    this.getNome(), this.getIdade(), this.getEmail(), this.getNomeEscola(), this.getId());
-        }
-    }
-    public void apagar(){
-        String comando = String.format("DELETE FROM Aluno WHERE id = id);", this.getId());
-
-    }
-
-    public int getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(String id) {
         this.id = id;
     }
 
@@ -87,14 +26,6 @@ public class Aluno extends _Default {
         this.nome = nome;
     }
 
-    public String getIdade() {
-        return idade;
-    }
-
-    public void setIdade(String idade) {
-        this.idade = idade;
-    }
-
     public String getEmail() {
         return email;
     }
@@ -103,11 +34,16 @@ public class Aluno extends _Default {
         this.email = email;
     }
 
-    public String getNomeEscola() {
-        return nomeEscola;
+    public String getPassword() {
+        return password;
     }
 
-    public void setNomeEscola(String nomeEscola) {
-        this.nomeEscola = nomeEscola;
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
     }
 }
--- app/src/main/java/com/example/boletimescolar/CadastroUsuarioActivity.java
+++ app/src/main/java/com/example/boletimescolar/CadastroUsuarioActivity.java
@@ -9,32 +9,69 @@ import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.UUID;
+
 public class CadastroUsuarioActivity extends AppCompatActivity {
 
     private ImageButton imageButton_voltarMainActivity;
     private Button button_salvarCadastro;
     private EditText inputText_Nome, inputText_Email, inputText_Password;
 
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_usuario);
+        inicializarFirebase();
 
         imageButton_voltarMainActivity = (ImageButton) findViewById(R.id.imageButton_voltarMainActivity);
         button_salvarCadastro = (Button) findViewById(R.id.button_salvarCadastro);
-
+        button_salvarCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                salvar();
+            }
+        });
         inputText_Nome = (EditText) findViewById(R.id.inputText_Nome);
         inputText_Email = (EditText) findViewById(R.id.inputText_email);
         inputText_Password = (EditText) findViewById(R.id.inputText_password);
 
+
     }
+    public void inicializarFirebase(){
+        FirebaseApp.initializeApp(CadastroUsuarioActivity.this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+    }
+    public void salvar(){
+        Aluno aluno = new Aluno();
+        aluno.setId(UUID.randomUUID().toString());
+        aluno.setNome(inputText_Nome.getText().toString());
+        aluno.setEmail(inputText_Email.getText().toString());
+        aluno.setPassword(inputText_Password.getText().toString());
+
+        databaseReference.child("Aluno").child(aluno.getId()) .setValue(aluno);
+        limparCampos();
+    }
+    public void limparCampos(){
+        inputText_Nome.setText("");
+        inputText_Email.setText("");
+        inputText_Password.setText("");
+    }
+
     //Metodo para voltar para a tela inicial do app (não logado/tela de cadastro e login)
     public void voltarMainActivity(View view) {
         finish();
     }
 
     //Metodo para enviar os dados do cadastro para o BD e ir para a tela início do app (não logado/tela de cadastro e login)
-    public void salvarCadastro(View view) {
+    public void salvarCadastro() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
